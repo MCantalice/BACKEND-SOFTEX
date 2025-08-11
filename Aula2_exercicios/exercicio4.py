abbc1a2a1a2b90f7700a94ecdf289e1e40e9a204
Maria Cantalice <mariaeunice94@hotmail.com>
2025-08-11 15:26:02 -0300

    Update exercicio4.py

--- Aula2_exercicios/exercicio4.py
+++ Aula2_exercicios/exercicio4.py
@@ -1,6 +1,6 @@
 nome= "Maria"
 sobrenome= "Cantalice"
-idade= "30"
+idade= 30 # nãoprecisa de aspa, pois aspas é uma string e um valor numérico sem soma sem nada pode usar sem as aspas
 
 print(nome)
 print(sobrenome)
